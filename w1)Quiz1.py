2c0072454342fc552f688c9cf019dcc3fd730484
minseo0823 <81514005+minseo0823@users.noreply.github.com>
2021-03-28 21:54:29 +0900

    Add files via upload

---
+++ w1)Quiz1.py
@@ -0,0 +1,8 @@
+# Quiz 변수를 이용하여 다음 문장을 출력하시오
+station1 = '사당'
+station2 = '신도림'
+station3 = '인천공항'
+
+print(station1 + '행 열차가 들어오고 있습니다')
+print(station2 + '행 열차가 들어오고 있습니다')
+print(station3 + '행 열차가 들어오고 있습니다')
